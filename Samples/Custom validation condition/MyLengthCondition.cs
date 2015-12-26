68673fbc344cf3ac39ae5fbddbab360b0ec974ed
kjac <kja@vertica.dk>
2015-12-26 10:46:31 +0100

    Clean up and improve samples and add validation condition sample

---
+++ Samples/Custom validation condition/MyLengthCondition.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using FormEditor.Fields;
+using FormEditor.Rendering;
+using FormEditor.Validation.Conditions;
+using Umbraco.Core.Models;
+
+namespace My.Conditions
+{
+	public class MyLengthCondition : Condition
+    {
+		// IMPORTANT: the condition must have a default constructor (or no constructor at all).
+		public MyLengthCondition()
+		{
+			// set up default values for condition configuration properties
+			LessThan = 100;
+		}
+
+		// this is the condition identifier towards Form Editor. it must be unique.
+		public override string Type
+	    {
+		    get { return "my.length"; }
+	    }
+
+		// this is the default condition name used in the validation UI (can be overridden by localization).
+		public override string PrettyName
+	    {
+		    get { return "Text length is less than"; }
+	    }
+
+		// these are the custom properties for our condition configuration. they must have public getters and setters.
+		public int LessThan { get; set; }
+
+		// test if the condition is met by a submitted field value
+	    public override bool IsMetBy(FieldWithValue fieldValue, IEnumerable<FieldWithValue> allCollectedFieldValues, IPublishedContent content)
+	    {
+			if(fieldValue == null || fieldValue.HasSubmittedValue == false)
+			{
+				// no such field or no submitted field value - the condition is met (LessThan will always be >= 1)
+				return false;
+			}
+
+			// the condition is met if the length of the submitted value is less than the value defined for LessThan
+		    return fieldValue.SubmittedValue.Length < LessThan;
+	    }
+
+		// override this to pass custom condition configuration parameters etc. to the frontend rendering.
+		// you can skip this if you do not need to pass anything (condition type is handled by the base class implementation).
+		public override ConditionData ForFrontEnd()
+		{
+			return new FieldConditionData(this);
+		}
+
+		// custom condition configuration container for frontend rendering. you can put whatever you like in this,
+		// just remember that all public properties are automatically serialized to JSON and made available to 
+		// the frontend rendering.
+		public class FieldConditionData : ConditionData
+		{
+			public FieldConditionData(MyLengthCondition condition)
+				: base(condition)
+			{
+				// map condition configuration properties
+				LessThan = condition.LessThan;
+			}
+
+			// these properties are passed to the frontend rendering
+			public int LessThan { get; private set; }
+		}
+    }
+}
--- Samples/Custom validation condition/Properties/AssemblyInfo.cs
+++ Samples/Custom validation condition/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("My.Fields")]
+[assembly: AssemblyTitle("My.Conditions")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("My.Fields")]
+[assembly: AssemblyProduct("My.Conditions")]
 [assembly: AssemblyCopyright("Copyright © Kenn Jacobsen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2e609250-0c82-472d-aee7-e7218330771c")]
+[assembly: Guid("0af97015-7c07-4fa7-8b8d-2519b5012654")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
