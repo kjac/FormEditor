b16ac92d8fd8098c4e13a3b424143aceb4a5c103
Kenn Jacobsen <kja@vertica.dk>
2016-09-28 14:09:24 +0200

    Move update to IStatisticsIndex
    Update will be part of the IStatisticsIndex from the beginning (it's
    already flagged as prone to change).

--- Source/Solution/FormEditor/Storage/IUpdateIndex.cs
+++ Source/Solution/FormEditor/Storage/IUpdateIndex.cs
@@ -6,7 +6,10 @@ namespace FormEditor.Storage
 	/// <summary>
 	/// This interface describes a Form Editor storage index that supports updating entries
 	/// </summary>
-	public interface IUpdateIndex : IIndex
+	/// <remarks>
+	/// In an upcoming release, this interface will be merged into IIndex
+	/// </remarks>
+	public interface IUpdateIndex
 	{
 		/// <summary>
 		/// Updates an entry in the index
@@ -15,14 +18,5 @@ namespace FormEditor.Storage
 		/// <param name="rowId">The ID of the entry to update</param>
 		/// <returns>The ID of the form entry</returns>
 		Guid Update(Dictionary<string, string> fields, Guid rowId);
-
-		/// <summary>
-		/// Updates an entry in the index
-		/// </summary>
-		/// <param name="fields">The field names and values to update</param>
-		/// <param name="fieldsValuesForStatistics">The field names and values to create statistics for</param>
-		/// <param name="rowId">The ID of the entry to update</param>
-		/// <returns>The ID of the form entry</returns>
-		Guid Update(Dictionary<string, string> fields, Dictionary<string, IEnumerable<string>> fieldsValuesForStatistics, Guid rowId);
 	}
 }
