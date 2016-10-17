25c453dd23841ade645682a85547b14f085b35e2
Kenn Jacobsen <kja@vertica.dk>
2016-10-17 15:10:24 +0200

    Fix multiple receipent email addresses
    WIP - untested!

---
+++ Source/Umbraco/Plugin/js/directives/multipleEmails.js
@@ -0,0 +1,29 @@
+﻿angular.module("umbraco.directives").directive("formEditorMultipleEmails", [
+  function () {
+    var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/;
+    return {
+      require: "ngModel",
+      restrict: "A",
+      link: function (scope, element, attrs, ctrl) {
+        ctrl.$parsers.unshift(function (viewValue) {
+          if (!viewValue) {
+            ctrl.$setValidity("formEditorMultipleEmails", true);
+            return viewValue;
+          }
+
+          var valid = _.every(viewValue.split(','), function (email) {
+            return EMAIL_REGEXP.test(email.trim());
+          });
+
+          if (valid) {
+            ctrl.$setValidity("formEditorMultipleEmails", true);
+            return viewValue;
+          } else {
+            ctrl.$setValidity("formEditorMultipleEmails", false);
+            return undefined;
+          }
+        });
+      }
+    };
+  }
+]);
