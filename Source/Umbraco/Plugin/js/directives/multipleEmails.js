46237d68b5485551e52c1b3c17ed3d2056893097
Kenn Jacobsen <kja@vertica.dk>
2016-10-18 08:23:28 +0200

    Fix email regex

--- Source/Umbraco/Plugin/js/directives/multipleEmails.js
+++ Source/Umbraco/Plugin/js/directives/multipleEmails.js
@@ -1,6 +1,6 @@
 ﻿angular.module("umbraco.directives").directive("formEditorMultipleEmails", [
   function () {
-    var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/;
+    var EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return {
       require: "ngModel",
       restrict: "A",
