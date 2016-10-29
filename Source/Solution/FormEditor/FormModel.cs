58e3f7a6ce236aa447594dad430120fc4fbcca05
kjac <kja@vertica.dk>
2016-10-29 12:29:14 +0200

    Introduce provider based handling of max submissions per user

--- Source/Solution/FormEditor/FormModel.cs
+++ Source/Solution/FormEditor/FormModel.cs
@@ -11,6 +11,7 @@ using FormEditor.Data;
 using FormEditor.Events;
 using FormEditor.Fields;
 using FormEditor.Fields.Statistics;
+using FormEditor.Limitations;
 using FormEditor.Storage;
 using FormEditor.Storage.Statistics;
 using Umbraco.Core.Models;
@@ -23,10 +24,9 @@ namespace FormEditor
 	{
 		public const string PropertyEditorAlias = @"FormEditor.Form";
 
-		private const string FormSubmittedCookieKey = "_fe";
-
 		private IEnumerable<Page> _pages;
 		private IEnumerable<Row> _rows;
+		private IMaxSubmissionsForCurrentUserHandler _maxSubmissionsForCurrentUserHandler;
 
 		#region Properties configured in the form editor
 
@@ -180,7 +180,7 @@ namespace FormEditor
 			// tell everyone that something was added
 			RaiseAfterAddToIndex(rowId, content);
 
-			SetFormSubmittedCookie(content);
+			MaxSubmissionsForCurrentUserHandler.HandleSubmission(this, content);
 
 			SendEmails(content, valueFields);
 
@@ -277,12 +277,7 @@ namespace FormEditor
 
 		public bool MaxSubmissionsExceededForCurrentUser(IPublishedContent content)
 		{
-			if (DisallowMultipleSubmissionsPerUser == false)
-			{
-				return false;
-			}
-			var cookie = Request.Cookies[FormSubmittedCookieKey];
-			return cookie != null && cookie.Value.Contains(FormSubmittedCookieValue(content));
+			return MaxSubmissionsForCurrentUserHandler.CanSubmit(this, content) == false;
 		}
 
 		public bool MaxSubmissionsExceeded()
@@ -374,6 +369,19 @@ namespace FormEditor
 				}
 				return Context.PublishedContentRequest.PublishedContent;
 			}
+		}
+
+		private IMaxSubmissionsForCurrentUserHandler MaxSubmissionsForCurrentUserHandler
+		{
+			get
+			{
+				if(_maxSubmissionsForCurrentUserHandler == null)
+				{
+					_maxSubmissionsForCurrentUserHandler = LimitationsHelper.GetMaxSubmissionsForCurrentUserHandler();
+				}
+				return _maxSubmissionsForCurrentUserHandler;
+			}
+			set { _maxSubmissionsForCurrentUserHandler = value; }
 		}
 
 		#region Stuff for backwards compatibility with v0.10.0.2 (before introducing form pages) - should probably be removed at some point
@@ -802,45 +810,6 @@ namespace FormEditor
 					? field.SubmittedValueForEmail()
 					: string.Empty;
 			});
-		}
-
-		private void SetFormSubmittedCookie(IPublishedContent content)
-		{
-			var cookieValue = (Request.Cookies.AllKeys.Contains(FormSubmittedCookieKey) ? Request.Cookies[FormSubmittedCookieKey].Value : null) ?? string.Empty;
-			var containsCurrentContent = cookieValue.Contains(FormSubmittedCookieValue(content));
-
-			if (DisallowMultipleSubmissionsPerUser == false)
-			{
-				if (containsCurrentContent)
-				{
-					// "only one submission per user" must've been enabled for this form at some point - explicitly remove the content ID from the cookie
-					cookieValue = cookieValue.Replace(FormSubmittedCookieValue(content), ",");
-					if (cookieValue == ",")
-					{
-						// this was the last content ID - remove the cookie 
-						Response.Cookies.Add(new HttpCookie(FormSubmittedCookieKey, cookieValue) { Expires = DateTime.Today.AddDays(-1) });
-					}
-					else
-					{
-						// update the cookie value
-						Response.Cookies.Add(new HttpCookie(FormSubmittedCookieKey, cookieValue) { Expires = DateTime.Today.AddDays(30) });						
-					}
-				}
-
-				return;
-			}
-
-			// add the content ID to the cookie value if it's not there already
-			if (containsCurrentContent == false)
-			{
-				cookieValue = string.Format("{0}{1}", cookieValue.TrimEnd(','), FormSubmittedCookieValue(content));
-			}
-			Response.Cookies.Add(new HttpCookie(FormSubmittedCookieKey, cookieValue) { Expires = DateTime.Today.AddDays(30) });
-		}
-
-		private static string FormSubmittedCookieValue(IPublishedContent content)
-		{
-			return string.Format(",{0},", content.Id);
 		}
 
 		#endregion
---
+++ Source/Solution/FormEditor/Limitations/LimitationsHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+
+namespace FormEditor.Limitations
+{
+	public class LimitationsHelper
+	{
+		public static IMaxSubmissionsForCurrentUserHandler GetMaxSubmissionsForCurrentUserHandler()
+		{
+			if(Configuration.Instance.MaxSubmissionsForCurrentUserHandlerType != null)
+			{
+				try
+				{
+					var handler = Activator.CreateInstance(Configuration.Instance.MaxSubmissionsForCurrentUserHandlerType) as IMaxSubmissionsForCurrentUserHandler;
+					if(handler == null)
+					{
+						throw new ConfigurationErrorsException(string.Format("Activator was unable to instantiate the custom MaxSubmissionsForCurrentUserHandler type \"{0}\"", Configuration.Instance.MaxSubmissionsForCurrentUserHandlerType.AssemblyQualifiedName));
+					}
+					return handler;
+				}
+				catch(Exception ex)
+				{
+					Log.Error(ex, "Could not create an instance of the custom MaxSubmissionsForCurrentUserHandler type");
+				}
+			}
+			// revert to default handler
+			return new MaxSubmissionsForCurrentUserHandler();
+		}
+
+	}
+}
