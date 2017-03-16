3c9286c12453e9853aaf80813f6808ec38664183
kjac <kja@vertica.dk>
2017-03-16 22:26:44 +0100

    First stab at WS integration

---
+++ Source/Solution/FormEditor/Integrations/WebServiceConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FormEditor.Integrations
+{
+	public class WebServiceConfiguration
+	{
+		public Uri Url { get; set; }
+
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+	}
+}
---
+++ Source/Umbraco/Plugin/js/controllers/config.webService.js
@@ -0,0 +1,20 @@
+﻿angular.module("umbraco").controller("FormEditor.Config.WebServiceController", ["$scope", "$filter", "assetsService", "angularHelper", 
+  function ($scope, $filter, assetsService, angularHelper) {
+    assetsService.loadCss("/App_Plugins/FormEditor/css/form.css");
+
+    $scope.model.value = $scope.model.value || defaultValue();
+
+    function defaultValue() {
+      return {
+        url: "",
+        userName: "",
+        password: ""
+      }
+    }
+
+    // helper to force the current form into the dirty state
+    $scope.setDirty = function () {
+      angularHelper.getCurrentForm($scope).$setDirty();
+    }
+  }
+]);
