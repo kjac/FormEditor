946ae8d0dc192d2ae38e0c96aa7698085b600e83
kjac <kja@vertica.dk>
2018-01-31 20:14:08 +0100

    Add a few Danish translations

--- Source/Umbraco/Plugin/js/langs/da-dk.js
+++ Source/Umbraco/Plugin/js/langs/da-dk.js
@@ -63,9 +63,9 @@
   "actions.header": "Handlinger",
   "actions.text": "Her kan du oprette handlinger, der viser eller skjuler felter baseret på andre felters værdi - f.eks. \"vis kun felt A hvis felt B ikke er tomt\".",
   "action.task.header": "...skal denne handling udføres:",
-  "action.task.theField": "",
-  "action.task.showField": "",
-  "action.task.hideField": "",
+  "action.task.theField": "feltet",
+  "action.task.showField": "Vis",
+  "action.task.hideField": "Skjul",
   "action.add": "Tilføj en handling",
   "action.text.noFields": "Du skal tilføje felter til formularen, før du kan oprette handlinger.",
 
